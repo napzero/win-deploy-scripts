ece8e2dfbc997aeed79ca2e2887be950e66d43a6
Matthew Podowski <napoleon.ski@gmail.com>
2019-10-21 16:36:01 -0500

    Update main.bat
    Replaced wtee with Windows PowerShell equivalent code.

--- jdk8/main.bat
+++ jdk8/main.bat
@@ -1,6 +1,9 @@
 @echo off
 cd /d %0\.. & color 0e & setlocal
 
-cmd /c install.bat | wtee %TEMP%\java_deployment.log
+::cmd /c install.bat | wtee %TEMP%\java_deployment.log
+
+ECHO One moment please...
+PowerShell.exe -Command "cmd /c install.bat | tee-object -filepath $env:TEMP\java_deployment.log"
 
 color & endlocal & EXIT 0
